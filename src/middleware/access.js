746a9468d33635d5eaee3a52a4a1132eb4268e32
indexpro24@gmail.com <indexpro24@gmail.com>
2021-05-04 21:30:45 +0300

    git init

--- src/middleware/access.js
+++ src/middleware/access.js
@@ -1,14 +1,10 @@
 export default function ({redirect, $auth} ) {
   if (!$auth.loggedIn) {
-  // if (!$auth.loggedIn) {
     console.log('Нет доступа!');
-    // console.log($auth.loggedIn)
     return redirect('/admin/login')
   }
   if ($auth.user.role !== 1) {
-  // if (!$auth.loggedIn) {
     console.log('Нет прав!');
-    // console.log($auth.loggedIn)
     return redirect('/admin/login')
   }
 }
