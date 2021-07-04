54d55017f966ed387b8d356935b7b9af8dfc3bdd
indexpro24@gmail.com <indexpro24@gmail.com>
2021-07-04 13:52:06 +0300

    git init

--- src/components/data/product/forms/create/index.vue
+++ src/components/data/product/forms/create/index.vue
@@ -162,42 +162,54 @@
                             @click.prevent="createProduct"
                             type="submit"
                             class="mt-6 inline-flex justify-center w-full py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-teal-900 hover:bg-teal-700 focus:outline-none focus:border-teal-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out">
-                            Добавить
+                            Добавить фото
                           </button>
-                          <p v-else
-                            class="mt-6 inline-flex justify-center w-full py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-teal-900 hover:bg-teal-700 focus:outline-none focus:border-teal-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out">
-                            Товар добавлен
-                          </p>
+<!--                          <p v-else-->
+<!--                            class="mt-6 inline-flex justify-center w-full py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-teal-900 hover:bg-teal-700 focus:outline-none focus:border-teal-700 focus:shadow-outline-indigo active:bg-green-700 transition duration-150 ease-in-out">-->
+<!--                            Товар добавлен-->
+<!--                          </p>-->
                         </span>
                   </div>
 
-                  <label for="first_name" class="block text-sm font-medium leading-5 text-gray-100">
-                    Изображение
-                  </label>
 
-                  <div class="m-2 sm:col-span-6">
-                    <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                      <div class="space-y-1 text-center">
-                        <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                          <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                        </svg>
-                        <div class="flex text-sm text-gray-600">
-                          <label for="file-upload"
-                                 class="mx-auto p-2 mx-1 relative cursor-pointer bg-white rounded-md font-medium text-blue-900 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                            <span>Загрузить фото</span>
-                            <input id="file-upload" name="image" type="file" class="sr-only" @change="onFileSelected">
-<!--                            <img :src="selectedFile" alt="">-->
-                          </label>
-<!--                          <p class="pl-1">или обрежьте</p>-->
+
+
+                  <div v-if="visibleCreateProduct">
+                    <label for="first_name" class="block text-sm font-medium leading-5 text-gray-100">
+                      Изображение
+                    </label>
+
+                    <div class="m-2 sm:col-span-6">
+                      <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                        <div class="space-y-1 text-center">
+                          <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                          </svg>
+                          <div class="flex text-sm text-gray-600">
+                            <label for="file-upload"
+                                   class="mx-auto p-2 mx-1 relative cursor-pointer bg-white rounded-md font-medium text-blue-900 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                              <span>Загрузить фото</span>
+                              <input id="file-upload" name="image" type="file" class="sr-only" @change="onFileSelected">
+                              <!--                            <img :src="selectedFile" alt="">-->
+                            </label>
+                            <!--                          <p class="pl-1">или обрежьте</p>-->
+                          </div>
+                          <p class="text-xs text-gray-500">
+                            PNG, JPG, GIF до 10MB
+                          </p>
+                          <hr>
+                          <ImageCropper :src="selectedFile"/>
                         </div>
-                        <p class="text-xs text-gray-500">
-                          PNG, JPG, GIF до 10MB
-                        </p>
-                        <hr>
-                        <ImageCropper :src="selectedFile"/>
                       </div>
                     </div>
+
                   </div>
+
+
+
+
+
+
                 </div>
               </div>
             </div>
