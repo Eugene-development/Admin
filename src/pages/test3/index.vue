eb2841e63afeac32ab63f2abe4d46da7f1521dcc
indexpro24@gmail.com <indexpro24@gmail.com>
2021-07-18 10:43:35 +0300

    git init

--- src/pages/test3/index.vue
+++ src/pages/test3/index.vue
@@ -1,81 +1,72 @@
 <template>
-  <vue-editor
-    ref="editor"
-    :placeholder="$props.placeholder"
-    :value="$props.content"
-    :editor-toolbar="toolbar"
-    :editor-options="settings"
-    :custom-modules="customModules"
-    use-custom-image-handler
-    @image-added="handleImageAdded"
-    @text-change="handleTextChange"
-  ></vue-editor>
+  <div class="container">
+    <div class="top">
+      <p>
+        <nuxt-link to="/">Back To Home</nuxt-link>
+        <button @click="reloadWindow" class="button--grey">Reload For Server Test</button>
+      </p>
+      <h2>Is SSR? {{isSSR}}</h2>
+    </div>
+
+    <no-ssr placeholder="Loading Your Editor...">
+      <vue-editor placeholder="Write Something..." v-model="content"></vue-editor>
+    </no-ssr>
+  </div>
 </template>
 
 <script>
-import { VueEditor } from 'vue2-editor'
+// let VueEditor
+
+// if (process.browser) {
+//   VueEditor = require('vue2-editor').VueEditor
+// }
 
 export default {
-  components: { VueEditor },
-  props: {
-    content: { type: String, default: '' },
-    placeholder: { type: String, default: '' },
+  asyncData() {
+    return {
+      content: "",
+      pageIsMounted: false,
+      isSSR: process.server ? true : false
+    };
   },
-  data: () => ({
-    loading: false,
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'],
-      [{ align: [] }],
-      ['blockquote', 'code-block'],
-      [{ header: [1, 2, 3, 4, 5, 6, false] }],
-      [{ color: [] }, { background: [] }],
-      [{ list: 'ordered' }, { list: 'bullet' }],
-      ['image' /* 'code-block' */],
-      ['clean'],
-    ],
-    customModules: [],
-    settings: {
-      modules: {
-        clipboard: {
-          matchers: [
-            [
-              Node.ELEMENT_NODE,
-              (node, delta) => {
-                delta.ops = delta.ops.map((op) => {
-                  return { insert: op.insert }
-                })
-                return delta
-              },
-            ],
-          ],
-        },
-      },
-    },
-  }),
+
+  // components: { VueEditor },
+
   methods: {
-    handleTextChange(delta, oldDelta, source) {
-      this.$emit('update:content', this.$refs.editor.quill.container.firstChild.innerHTML)
-    },
-    async handleImageAdded(file, Editor, cursorLocation, resetUploader) {
-      try {
-        this.loading = true
-
-        const formData = new FormData()
-
-        formData.append('image', file)
-
-        const { data } = await this.$axios.post(`/wysiwyg/image`, formData) // аплоад картинки на сервер
-
-        Editor.insertEmbed(cursorLocation, 'image', data.url)
-        Editor.formatText(cursorLocation, 1, 'alt', 'image') // Знчение для alt-аттрибута изображения (SEO)
-
-        resetUploader()
-      } catch (error) {
-        console.error(error)
-      } finally {
-        this.loading = false
-      }
-    },
-  },
-}
+    reloadWindow() {
+      window.location.reload(true);
+    }
+  }
+};
 </script>
+
+<style scoped>
+button {
+  cursor: pointer;
+}
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: left;
+}
+h1 {
+  font-weight: 400;
+  margin-bottom: -0.5em;
+}
+.name {
+  color: #3b8070;
+}
+p {
+  margin-top: 20px;
+}
+.top {
+  margin-bottom: 2.5em;
+  text-align: center;
+}
+button {
+  font-size: 16px;
+  font-weight: 900;
+}
+</style>
