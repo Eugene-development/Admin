e20b8d2073468366c659f9b53e077c819645b33a
indexpro24@gmail.com <indexpro24@gmail.com>
2021-08-24 21:14:11 +0300

    git init

--- src/store/myAuth/token/index.js
+++ src/store/myAuth/token/index.js
@@ -1,6 +1,7 @@
 export const actions = {
   async setToken ({ $auth }) {
+    console.log(window.location.hostname);
     const token = this.$auth.user.project_id;
     this.$axios.setToken(token, 'Bearer')
-  }
+  },
 };
