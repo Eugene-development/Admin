9e084d5b2e2657aa9fee26f9072aafff2cec46ed
indexpro24@gmail.com <indexpro24@gmail.com>
2021-10-26 22:55:42 +0300

    git init

--- src/components/data/product/cards/index.vue
+++ src/components/data/product/cards/index.vue
@@ -56,16 +56,16 @@
 
                       Highlighted: "text-white bg-indigo-600", Not Highlighted: "text-gray-900"
                     -->
-                    <li v-for="category of allCategory" :key="category.id" class="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9 overflow-auto focus:outline-none" id="listbox-option-0" role="option">
+                    <li v-for="category of allCategory" :key="category.id" class="text-gray-900 relative py-2 pl-3 pr-9 overflow-auto focus:outline-none" id="listbox-option-0" role="option">
                       <!-- Selected: "font-semibold", Not Selected: "font-normal" -->
-                      <span class="text-gray-900 font-normal hover:text-2xl cursor-pointer select-none relative py-2 pl-8 pr-4 hover:text-red-900">{{ category.name }}</span>
+                      <span class="text-gray-900 font-normal hover:text-2xl cursor-pointer relative py-2 pl-8 pr-4 hover:text-red-900">{{ category.name }}</span>
 
                       <!--
                         Checkmark, only display for selected option.
 
                         Highlighted: "text-white", Not Highlighted: "text-indigo-600"
                       -->
-<!--                      <span class="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4">-->
+<!--                      <span class="text-red-600 absolute inset-y-0 right-0 flex items-center pr-4">-->
 <!--                        &lt;!&ndash; Heroicon name: solid/check &ndash;&gt;-->
 <!--                        <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">-->
 <!--                          <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />-->
